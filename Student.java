23b495049f7feff820c17360201847d67d733262
rajapatel02 <rajapatel02011@gmail.com>
2024-04-15 20:51:35 +0530

    add student class

---
+++ Student.java
@@ -0,0 +1,17 @@
+package javacoderepo;
+
+public class Student {
+
+	String name;
+	int id;
+	String address;
+	 
+	Student (int id, String name, String address){
+		this.id=id;
+		this.name=name;
+		this.address=address;
+	}
+	Student(){
+		
+	}
+}
